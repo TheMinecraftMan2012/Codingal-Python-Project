f04c40bb9e6af24cb12a82dc92462e2817ca3e0a
TheMinecraftMan2012 <h.codingal@gmail.com>
2024-12-24 08:49:36 +0600

    commit

--- After Class Project/Power Calculator/main.py
+++ After Class Project/Power Calculator/main.py
@@ -1,3 +1,6 @@
 a = int(input("Enter the base for powers: "))
-n = int(input("Enter the starting point of power: "))
+n = int(input("Enter the range point of power: "))
 
+for i in range(1, n + 1):
+    power = a**i
+    print("{0} to the power {1} is {2}".format(a, i, power))
