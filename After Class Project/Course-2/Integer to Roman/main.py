2d25b011e0d30b07ff0a9c01c75c4b072e6a75b7
TheMinecraftMan2012 <h.codingal@gmail.com>
2025-03-13 09:49:23 +0600

    commit

--- After Class Project/Course-2/Integer to Roman/main.py
+++ After Class Project/Course-2/Integer to Roman/main.py
@@ -3,14 +3,21 @@ class roman_conversion:
         self.number = number
     
     def convert(self):
-        if self.number >= 4000:
-            print("Can't convert roman because number is equal to or greater than 4000")
+        if self.number <= 0 or self.number >= 400:
+            return "Invalid: Roman number can convert 1 to 3999"
         else:
-            number = str(self.number)
-            num_len = len(number)
-            if num_len == 4:
-                if number[0] == 1:
-                    pass
+            roman_numerals = {
+            1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L",
+            90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"
+            }
+    
+            roman_str = ""
+            for value in sorted(roman_numerals.keys(), reverse=True):
+                while self.number >= value:
+                    roman_str += roman_numerals[value]
+                    self.number -= value
+            return roman_str
 
-conv = roman_conversion(int(input("Enter your number: ")))
-conv.convert()
+num = int(input("Enter your number: "))
+conv = roman_conversion(num)
+print(conv.convert())
